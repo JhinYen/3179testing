62fa0ed233428833be5fb439e837009bac30aa41
JhinYen <jlow0050@student.monash.edu>
2025-10-16 18:54:26 +0800

    improve a lot of stuff after critique

--- DV2_final/vega_lite_vis.js
+++ DV2_final/vega_lite_vis.js
@@ -5,4 +5,8 @@ vegaEmbed("#infant_dual", "infant_mortality_dual.vg.json");
 vegaEmbed("#econ_box", "economic_boxplot.vg.json");
 vegaEmbed("#vaccine_bar", "vaccination_coverage.vg.json");
 vegaEmbed("#fertility_life_scatter", "fertility_vs_life.vg.json");
-// vegaEmbed("#thinness_schooling_parallel", "thinness_schooling_parallel.vg.json");
+vegaEmbed("#migration_arc", "migration_arc.vg.json");
+vegaEmbed("#pop_treemap", "population_treemap.vg.json");
+
+
+vegaEmbed("#testing", "testing.vg.json");
